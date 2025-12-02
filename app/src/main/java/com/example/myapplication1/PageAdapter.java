0f80a6fe696ab140647f10d76f23c1ae9679fd9f
Anyutaa05 <145389530+Anyutaa05@users.noreply.github.com>
2025-12-02 10:58:09 +0200

    Initial commit

---
+++ app/src/main/java/com/example/myapplication1/PageAdapter.java
@@ -0,0 +1,31 @@
+package com.example.myapplication1;
+
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+public class PageAdapter extends FragmentStateAdapter {
+
+    public PageAdapter(@NonNull androidx.fragment.app.FragmentActivity fa) {
+        super(fa);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        switch (position) {
+            case 0:
+                return PageFragment.newInstance("Сторінка 1: Привіт, Android!", 0xFFE3F2FD);
+            case 1:
+                return PageFragment.newInstance("Сторінка 2: Тут ваше завдання", 0xFFE8F5E9);
+            default:
+                return PageFragment.newInstance("Сторінка 3: Свайп ліворуч/праворуч", 0xFFFFF3E0);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return 3;
+    }
+}
